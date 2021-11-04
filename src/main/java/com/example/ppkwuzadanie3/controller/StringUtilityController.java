0195671c0737799d767639c0410a58e84054c40b
dawidsadowski <dsadowski@craftware.biz>
2021-11-04 14:14:03 +0100

    Add different mappings for different formats

--- src/main/java/com/example/ppkwuzadanie3/controller/StringUtilityController.java
+++ src/main/java/com/example/ppkwuzadanie3/controller/StringUtilityController.java
@@ -3,18 +3,25 @@ package com.example.ppkwuzadanie3.controller;
 import com.example.ppkwuzadanie3.StringUtility;
 import org.json.JSONException;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class StringUtilityController {
 
-    @GetMapping(value = "/", produces = {MediaType.APPLICATION_XML_VALUE})
-    String getStringUtility(@RequestParam String text, @RequestParam String format) throws JSONException {
-        return StringUtility.jsonToFormat(text, format);
+    @GetMapping(value = "/json/{text}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    String getJsonString(@PathVariable String text) throws JSONException {
+        return StringUtility.jsonToFormat(text, "json");
+    }
+
+    @GetMapping(value = "/xml/{text}", produces = {MediaType.APPLICATION_XML_VALUE})
+    String getXmlString(@PathVariable String text) throws JSONException {
+        return StringUtility.jsonToFormat(text, "xml");
+    }
+
+    @GetMapping(value = "/csv/{text}")
+    String getCsvString(@PathVariable String text) throws JSONException {
+        return StringUtility.jsonToFormat(text, "csv");
     }
 
 }
